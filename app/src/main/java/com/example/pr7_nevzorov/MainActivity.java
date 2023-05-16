a0c4b4b4f443cac92d33976b2ef88c31c608c297
youut <nevzorovurij880@gmail.com>
2023-05-16 15:28:32 +0700

    start

---
+++ app/src/main/java/com/example/pr7_nevzorov/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pr7_nevzorov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
